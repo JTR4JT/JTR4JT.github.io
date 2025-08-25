6ba2795c580bb49fb5f536a45b62d75918fa407d
JTR4JT <2458638823@qq.com>
2025-08-25 23:28:46 +0800

    Site updated: 2025-08-25 23:28:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/25/我的第一篇博客/","2025/08/25/asp.net/","2025/08/25/hello-world/"];function toRandomPost(){
+var posts=["2025/08/25/asp.net/","2025/08/25/hello-world/","2025/08/25/我的第一篇博客/","2025/08/25/Spring Cloud笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
